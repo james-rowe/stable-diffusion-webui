367e669291a9229e6c826c43abeba6b64adfb0b1
James Rowe <james@MacBook-Pro.local>
2023-08-19 16:18:00 -0500

    default UI port to 3001

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--api --no-download-sd-model"
+export COMMANDLINE_ARGS="--api --no-download-sd-model --port 3001"
 
 # python3 executable
 #python_cmd="python3"
