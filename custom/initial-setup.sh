00f8da18e7df44a59a26c95a6d000c35d0faafeb
James Rowe <james@MacBook-Pro.local>
2023-08-29 01:29:08 -0500

    Fixing server startup scripts

--- custom/initial-setup.sh
+++ custom/initial-setup.sh
@@ -7,6 +7,13 @@ cd /workspace/stable-diffusion-webui/models/Stable-diffusion && curl -L -o main.
 cd /workspace/stable-diffusion-webui/models/VAE && curl -L -o main.ckpt https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.ckpt
 cd /workspace/stable-diffusion-webui/models/hypernetworks && curl -L -o main.pt https://civitai.com/api/download/models/6029
 
+cd /workspace/
+python3 -m venv chatfriends
+source chatfriends/bin/activate
+pip install -r requirements.txt
+curl ifconfig.me
+
+
 # cp /workspace/stable-diffusion-webui/custom/stable-diffusion.service /etc/systemd/system/
 # cp /workspace/stable-diffusion-webui/custom/fastapi.service /etc/systemd/system/
 
--- custom/startup-server.sh
+++ custom/startup-server.sh
@@ -1,4 +1,4 @@
 python3 -m venv chatfriends
-source chatfriends/bin/activate
+source /workspace/chatfriends/bin/activate
 cd /workspace/ && nohup python -m uvicorn main:app --host 0.0.0.0 --port 8000
 cd /workspace/stable-diffusion-webui && nohup ./webui.sh
