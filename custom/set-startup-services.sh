d5d5b2742af541a78fe452614688aed325e87905
James Rowe <james@MacBook-Pro.local>
2023-08-30 23:33:40 -0500

    fix server startup scripts

--- custom/set-startup-services.sh
+++
@@ -1,7 +0,0 @@
-# copy service files to /etc/systemd/system
-cp /workspace/stable-diffusion-webui/custom/stable-diffusion.service /etc/systemd/system/
-cp /workspace/stable-diffusion-webui/custom/fastapi.service /etc/systemd/system/
-systemctl enable stable-diffusion
-systemctl enable fastapi
-systemctl start stable-diffusion
-systemctl start fastapi
--- custom/startup-server.sh
+++ custom/startup-server.sh
@@ -1,4 +1,7 @@
-python3 -m venv chatfriends
+#!/bin/bash
+set -e
+
+python3 -m venv /workspace/chatfriends
 source /workspace/chatfriends/bin/activate
-cd /workspace/ && nohup python -m uvicorn main:app --host 0.0.0.0 --port 8000
-cd /workspace/stable-diffusion-webui && nohup ./webui.sh
+cd /workspace/ && nohup python -m uvicorn main:app --host 0.0.0.0 --port 8000 > /workspace/uvicorn_output.txt &
+cd /workspace/stable-diffusion-webui && nohup ./webui.sh > /workspace/webui_output.txt &
